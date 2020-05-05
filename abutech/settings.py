145d0390d3fe4c61af9e0238bd58ef4af3bc9b90
shoxruxmirzoo <shokhrukhmirzo2403@gmail.com>
2020-05-06 01:55:55 +0500

    dfgdfg

--- abutech/settings.py
+++ abutech/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,3 +123,5 @@ USE_TZ = True
 TOKEN = "1242291792:AAHtu_DcIoZ67nU3uMDymM_SHKcRP1-QSII"
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
